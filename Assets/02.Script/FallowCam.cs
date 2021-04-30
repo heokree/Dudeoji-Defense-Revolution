2f35509e9341f6a66f1edf2faf9ee2945b3841c4
Heokree Lim <heokree.lim@gmail.com>
2021-04-30 14:26:29 +0900

    Asset Ready

---
+++ Assets/02.Script/FallowCam.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallowCam : MonoBehaviour
+{
+    public Transform targetTr;
+
+    public float distance   = 5.0f;
+    public float height     = 5.0f;
+    public float damping    = 10.0f;
+
+    private Transform cameraTr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 cameraPosition = targetTr.position + (Vector3.forward * -1 * distance) + (Vector3.up * height);
+
+        cameraTr.position = Vector3.Lerp(cameraTr.position, cameraPosition, Time.deltaTime * damping);
+
+        cameraTr.LookAt(targetTr.position);
+    }
+}
